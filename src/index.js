f0839ccc95ee94b81a8530d8174bd724ffc48a04
Rogue-Ram <82464257+Rogue-Ram@users.noreply.github.com>
2023-02-10 22:51:47 -0600

    updated some stuff

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.static(publicPath));
 // Load vendor files last.
 // The vendor's uv.config.js won't conflict with our uv.config.js inside the publicPath directory.
-app.use("/uv/", express.static(uvPath));
+app.use("/p/", express.static(uvPath));
 
 // Error for everything else
 app.use((req, res) => {
@@ -48,14 +48,7 @@ server.on("listening", () => {
 
   // by default we are listening on 0.0.0.0 (every interface)
   // we just need to list a few
-  console.log("Listening on:");
-  console.log(`\thttp://localhost:${address.port}`);
-  console.log(`\thttp://${hostname()}:${address.port}`);
-  console.log(
-    `\thttp://${
-      address.family === "IPv6" ? `[${address.address}]` : address.address
-    }:${address.port}`
-  );
+
 });
 
 server.listen({
